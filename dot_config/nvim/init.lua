d580bd9b28fee696f1f44fb5640bc8c3a641444a
lsdevcloud <info@lsdevcloud.com>
2025-02-26 10:01:00 +0100

    Switch to lazy.nvim plugin manager

--- dot_config/nvim/init.lua
+++ dot_config/nvim/init.lua
@@ -1,4 +1,42 @@
--- disable mouse 
+-- Bootstrap lazy.nvim
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not (vim.uv or vim.loop).fs_stat(lazypath) then
+  local lazyrepo = "https://github.com/folke/lazy.nvim.git"
+  local out = vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
+  if vim.v.shell_error ~= 0 then
+    vim.api.nvim_echo({
+      { "Failed to clone lazy.nvim:\n", "ErrorMsg" },
+      { out, "WarningMsg" },
+      { "\nPress any key to exit..." },
+    }, true, {})
+    vim.fn.getchar()
+    os.exit(1)
+  end
+end
+vim.opt.rtp:prepend(lazypath)
+
+-- Make sure to setup `mapleader` and `maplocalleader` before
+-- loading lazy.nvim so that mappings are correct.
+-- This is also a good place to setup other settings (vim.opt)
+vim.g.mapleader = " "
+vim.g.maplocalleader = "\\"
+
+-- Setup lazy.nvim
+require("lazy").setup({
+  spec = {
+    -- List of plugins
+    { "nvim-tree/nvim-tree.lua", dependencies = { "nvim-tree/nvim-web-devicons" } },
+    { "iamcco/markdown-preview.nvim" },
+    { "catppuccin/nvim" },  -- Add the Catppuccin colorscheme
+  },
+  -- Configure any other settings here. See the documentation for more details.
+  -- colorscheme that will be used when installing plugins.
+  install = { colorscheme = { "habamax" } },
+  -- automatically check for plugin updates
+  checker = { enabled = true },
+})
+
+-- disable mouse
 vim.opt.mouse = ""
 
 -- nvim tree toggle
@@ -34,44 +72,14 @@ require("nvim-tree").setup({
   },
 })
 
--- install packer if not installed already
-local install_path = vim.fn.stdpath('data')..'/site/pack/packer/start/packer.nvim'
-if not vim.loop.fs_stat(install_path) then
-  vim.fn.system({'git', 'clone', 'https://github.com/wbthomason/packer.nvim', install_path})
-end
- 
--- packer
-vim.cmd [[packadd packer.nvim]]
-
 -- open NvimTree on start
 vim.cmd [[
     autocmd VimEnter * NvimTreeOpen
     autocmd VimEnter * ++nested wincmd w
 ]]
 
+-- Set colorscheme (catppuccin-mocha if available, default otherwise)
 local ok, _ = pcall(vim.cmd, 'colorscheme catppuccin-mocha')
 if not ok then
   vim.cmd 'colorscheme default'
 end
-
-require('packer').startup(function(use)
-  	use 'wbthomason/packer.nvim'
-	use 'sbdchd/neoformat'
-
--- markdown preview plugin
-use {
-    'iamcco/markdown-preview.nvim',
-    "catppuccin/nvim", as = "catppuccin"
-}
-
-
--- tree plugin
-use {
-
-    'nvim-tree/nvim-tree.lua',
-    requires = {
-        'nvim-tree/nvim-web-devicons', -- optional
-    },
-}
-
-end)
