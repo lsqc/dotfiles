67d392794dfd569adf1632ccedec641baacb2002
lsdevcloud <lesc@lsdevcloud.net>
2025-03-08 00:05:35 +0100

    [neovim]: create custom config file for use on t440p (add tab plugin etc)

--- dot_config/nvim/init.lua
+++ dot_config/nvim/init.lua
@@ -1,4 +1,4 @@
--- lazy.nvim
+-- Bootstrap lazy.nvim
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not (vim.uv or vim.loop).fs_stat(lazypath) then
   local lazyrepo = "https://github.com/folke/lazy.nvim.git"
@@ -23,13 +23,21 @@ require("lazy").setup({
   spec = {
     { "nvim-tree/nvim-tree.lua", dependencies = { "nvim-tree/nvim-web-devicons" } },
     { "iamcco/markdown-preview.nvim" },
-    { "catppuccin/nvim" },
+    { "catppuccin/nvim" },  -- Add the Catppuccin colorscheme
+    {'romgrk/barbar.nvim',
+        dependencies = {
+            'lewis6991/gitsigns.nvim', -- OPTIONAL: for git status
+            'nvim-tree/nvim-web-devicons', -- OPTIONAL: for file icons
+        },
+        init = function() vim.g.barbar_auto_setup = false end,
+        opts = {
+        },
+        version = '^1.0.0', -- optional: only update when a new 1.x version is released
+    },
   },
   install = { colorscheme = { "habamax" } },
-  -- automatically check for plugin updates
   checker = { enabled = true },
 })
-
 
 -- disable mouse
 vim.opt.mouse = ""
@@ -74,8 +82,43 @@ vim.cmd [[
 ]]
 
 -- Set colorscheme (catppuccin-mocha if available, default otherwise)
---local ok, _ = pcall(vim.cmd, 'colorscheme catppuccin-mocha')
+local ok, _ = pcall(vim.cmd, 'colorscheme catppuccin-mocha')
 if not ok then
   vim.cmd 'colorscheme default'
 end
 
+-- hotkeys
+local map = vim.api.nvim_set_keymap
+local opts = { noremap = true, silent = true }
+
+-- Move to previous/next
+map('n', '<Space>,', '<Cmd>BufferPrevious<CR>', opts)
+map('n', '<Space>.', '<Cmd>BufferNext<CR>', opts)
+
+-- Re-order to previous/next
+map('n', '<Space><', '<Cmd>BufferMovePrevious<CR>', opts)
+map('n', '<Space>>', '<Cmd>BufferMoveNext<CR>', opts)
+
+-- Goto buffer in position...
+map('n', '<Space>1', '<Cmd>BufferGoto 1<CR>', opts)
+map('n', '<Space>2', '<Cmd>BufferGoto 2<CR>', opts)
+map('n', '<Space>3', '<Cmd>BufferGoto 3<CR>', opts)
+map('n', '<Space>4', '<Cmd>BufferGoto 4<CR>', opts)
+map('n', '<Space>5', '<Cmd>BufferGoto 5<CR>', opts)
+map('n', '<Space>6', '<Cmd>BufferGoto 6<CR>', opts)
+map('n', '<Space>7', '<Cmd>BufferGoto 7<CR>', opts)
+map('n', '<Space>8', '<Cmd>BufferGoto 8<CR>', opts)
+map('n', '<Space>9', '<Cmd>BufferGoto 9<CR>', opts)
+map('n', '<Space>0', '<Cmd>BufferLast<CR>', opts)
+
+-- Pin/unpin buffer
+map('n', '<Space>p', '<Cmd>BufferPin<CR>', opts)
+
+-- Close buffer
+map('n', '<Space>c', '<Cmd>BufferClose<CR>', opts)
+
+-- Magic buffer-picking mode
+
+
+-- hotkey for leaving terminal mode
+vim.api.nvim_set_keymap('t', '<C-n>', '<C-\\><C-n>', { noremap = true, silent = true })
