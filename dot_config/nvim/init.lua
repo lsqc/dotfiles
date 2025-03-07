1a3ed335177ecdd9309e8f97d29e322fa704fea1
lsdevcloud <info@lsdevcloud.com>
2025-03-07 23:43:27 +0100

    [neovim]: add plugin for tabs (https://github.com/romgrk/barbar.nvim)

--- dot_config/nvim/init.lua
+++ dot_config/nvim/init.lua
@@ -15,24 +15,27 @@ if not (vim.uv or vim.loop).fs_stat(lazypath) then
 end
 vim.opt.rtp:prepend(lazypath)
 
--- Make sure to setup `mapleader` and `maplocalleader` before
--- loading lazy.nvim so that mappings are correct.
--- This is also a good place to setup other settings (vim.opt)
 vim.g.mapleader = " "
 vim.g.maplocalleader = "\\"
 
 -- Setup lazy.nvim
 require("lazy").setup({
   spec = {
-    -- List of plugins
     { "nvim-tree/nvim-tree.lua", dependencies = { "nvim-tree/nvim-web-devicons" } },
     { "iamcco/markdown-preview.nvim" },
     { "catppuccin/nvim" },  -- Add the Catppuccin colorscheme
+    {'romgrk/barbar.nvim',
+        dependencies = {
+            'lewis6991/gitsigns.nvim', -- OPTIONAL: for git status
+            'nvim-tree/nvim-web-devicons', -- OPTIONAL: for file icons
+        },
+        init = function() vim.g.barbar_auto_setup = false end,
+        opts = {
+        },
+        version = '^1.0.0', -- optional: only update when a new 1.x version is released
+    },
   },
-  -- Configure any other settings here. See the documentation for more details.
-  -- colorscheme that will be used when installing plugins.
   install = { colorscheme = { "habamax" } },
-  -- automatically check for plugin updates
   checker = { enabled = true },
 })
 
@@ -83,3 +86,36 @@ local ok, _ = pcall(vim.cmd, 'colorscheme catppuccin-mocha')
 if not ok then
   vim.cmd 'colorscheme default'
 end
+
+-- hotkeys
+local map = vim.api.nvim_set_keymap
+local opts = { noremap = true, silent = true }
+
+-- Move to previous/next
+map('n', '<Space>,', '<Cmd>BufferPrevious<CR>', opts)
+map('n', '<Space>.', '<Cmd>BufferNext<CR>', opts)
+
+-- Re-order to previous/next
+map('n', '<Space><', '<Cmd>BufferMovePrevious<CR>', opts)
+map('n', '<Space>>', '<Cmd>BufferMoveNext<CR>', opts)
+
+-- Goto buffer in position...
+map('n', '<Space>1', '<Cmd>BufferGoto 1<CR>', opts)
+map('n', '<Space>2', '<Cmd>BufferGoto 2<CR>', opts)
+map('n', '<Space>3', '<Cmd>BufferGoto 3<CR>', opts)
+map('n', '<Space>4', '<Cmd>BufferGoto 4<CR>', opts)
+map('n', '<Space>5', '<Cmd>BufferGoto 5<CR>', opts)
+map('n', '<Space>6', '<Cmd>BufferGoto 6<CR>', opts)
+map('n', '<Space>7', '<Cmd>BufferGoto 7<CR>', opts)
+map('n', '<Space>8', '<Cmd>BufferGoto 8<CR>', opts)
+map('n', '<Space>9', '<Cmd>BufferGoto 9<CR>', opts)
+map('n', '<Space>0', '<Cmd>BufferLast<CR>', opts)
+
+-- Pin/unpin buffer
+map('n', '<Space>p', '<Cmd>BufferPin<CR>', opts)
+
+-- Close buffer
+map('n', '<Space>c', '<Cmd>BufferClose<CR>', opts)
+
+-- Magic buffer-picking mode
+
