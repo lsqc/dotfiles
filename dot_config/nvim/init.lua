c3c3d82ad9b836b7b69bfe7ec8482f520fc4f8b8
lsdevcloud <lesc@lsdevcloud.net>
2025-03-10 08:39:41 +0100

    [neovim]: cleanup (remove duplicate lines, remove unneccessary comments etc)

--- dot_config/nvim/init.lua
+++ dot_config/nvim/init.lua
@@ -1,10 +1,8 @@
-vim.g.loaded_netrw       = 1
+vim.g.loaded_netrw = 1
 vim.g.loaded_netrwPlugin = 1
 
--- disable mouse
 vim.opt.mouse = ""
 
--- Bootstrap lazy.nvim
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not (vim.uv or vim.loop).fs_stat(lazypath) then
   local lazyrepo = "https://github.com/folke/lazy.nvim.git"
@@ -24,48 +22,34 @@ vim.opt.rtp:prepend(lazypath)
 vim.g.mapleader = " "
 vim.g.maplocalleader = "\\"
 
--- Setup lazy.nvim
 require("lazy").setup({
   spec = {
     { "nvim-tree/nvim-tree.lua", dependencies = { "nvim-tree/nvim-web-devicons" } },
     { "iamcco/markdown-preview.nvim" },
+    { "folke/tokyonight.nvim", lazy = false, priority = 1000, opts = {} },
     { "catppuccin/nvim" },  -- Add the Catppuccin colorscheme
-    {'romgrk/barbar.nvim',
-        dependencies = {
-            'lewis6991/gitsigns.nvim', -- OPTIONAL: for git status
-            'nvim-tree/nvim-web-devicons', -- OPTIONAL: for file icons
-        },
-        init = function() vim.g.barbar_auto_setup = false end,
-        opts = {
-        },
-        version = '^1.0.0', -- optional: only update when a new 1.x version is released
+    {
+      'romgrk/barbar.nvim',
+      dependencies = {
+        'lewis6991/gitsigns.nvim', -- OPTIONAL: for git status
+        'nvim-tree/nvim-web-devicons', -- OPTIONAL: for file icons
+      },
+      init = function() vim.g.barbar_auto_setup = false end,
+      opts = {},
+      version = '^1.0.0',
     },
   },
   install = { colorscheme = { "habamax" } },
   checker = { enabled = true },
 })
 
--- disable mouse
-vim.opt.mouse = ""
-
--- nvim tree toggle
-vim.api.nvim_set_keymap('n', '<Space>t', ':NvimTreeToggle<CR>', { noremap = true, silent = true })
-
--- use space+w for window switching
-vim.api.nvim_set_keymap('n', '<Space>w', '<C-w>w', { noremap = true, silent = true })
-
--- tab indent configuration
-vim.o.tabstop = 4 -- set tab character to 4 spaces
-vim.o.expandtab = true -- insert spaces instead of tab character
-vim.o.softtabstop = 4 -- amount of spaces inserted (replaces tab character)
-vim.o.shiftwidth = 4 -- amount of spaces inserted when indenting
-
-vim.g.loaded_netrw = 1
-vim.g.loaded_netrwPlugin = 1
+vim.o.tabstop = 4
+vim.o.expandtab = true
+vim.o.softtabstop = 4
+vim.o.shiftwidth = 4
 
 vim.opt.termguicolors = true
 
--- config for nvim tree
 require("nvim-tree").setup({
   sort = {
     sorter = "case_sensitive",
@@ -81,48 +65,27 @@ require("nvim-tree").setup({
   },
 })
 
--- open NvimTree on start
-vim.cmd [[
-    autocmd VimEnter * NvimTreeOpen
-local function open_nvim_tree()
+vim.cmd [[autocmd VimEnter * NvimTreeOpen]]
 
-  -- open the tree
-  require("nvim-tree.api").tree.open()
-end
-
-vim.api.nvim_create_autocmd({ "VimEnter" }, { callback = open_nvim_tree })
-
-vim.cmd [[
-    autocmd VimEnter * ++nested wincmd w
-]]
-
--- Set colorscheme (catppuccin-mocha if available, default otherwise)
 local ok, _ = pcall(vim.cmd, 'colorscheme catppuccin-mocha')
 if not ok then
   vim.cmd 'colorscheme default'
 end
 
--- override theme background color with terminal default
 vim.cmd('highlight Normal guibg=NONE ctermbg=NONE')
 
--- set nvimtree background
 vim.cmd('highlight NvimTreeNormal guibg=NONE ctermbg=NONE')
 vim.cmd('highlight NvimTreeVertSplit guibg=NONE ctermbg=NONE')
 
-
--- hotkeys
 local map = vim.api.nvim_set_keymap
 local opts = { noremap = true, silent = true }
 
--- Move to previous/next
+map('n', '<Space>t', ':NvimTreeToggle<CR>', opts)
+map('n', '<Space>w', '<C-w>w', opts)
 map('n', '<Space>,', '<Cmd>BufferPrevious<CR>', opts)
 map('n', '<Space>.', '<Cmd>BufferNext<CR>', opts)
-
--- Re-order to previous/next
 map('n', '<Space><', '<Cmd>BufferMovePrevious<CR>', opts)
 map('n', '<Space>>', '<Cmd>BufferMoveNext<CR>', opts)
-
--- Goto buffer in position...
 map('n', '<Space>1', '<Cmd>BufferGoto 1<CR>', opts)
 map('n', '<Space>2', '<Cmd>BufferGoto 2<CR>', opts)
 map('n', '<Space>3', '<Cmd>BufferGoto 3<CR>', opts)
@@ -133,12 +96,8 @@ map('n', '<Space>7', '<Cmd>BufferGoto 7<CR>', opts)
 map('n', '<Space>8', '<Cmd>BufferGoto 8<CR>', opts)
 map('n', '<Space>9', '<Cmd>BufferGoto 9<CR>', opts)
 map('n', '<Space>0', '<Cmd>BufferLast<CR>', opts)
-
--- Pin/unpin buffer
 map('n', '<Space>p', '<Cmd>BufferPin<CR>', opts)
-
--- Close buffer
 map('n', '<Space>c', '<Cmd>BufferClose<CR>', opts)
 
--- hotkey for leaving terminal mode
 vim.api.nvim_set_keymap('t', '<C-n>', '<C-\\><C-n>', { noremap = true, silent = true })
+
