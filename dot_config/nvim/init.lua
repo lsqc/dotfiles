ebe7e806e8ac4fcf4160be1eb8db99eb5548e5f9
lsdevcloud <info@lsdevcloud.com>
2025-03-30 23:56:52 +0200

    neovim: add lualine

--- dot_config/nvim/init.lua
+++ dot_config/nvim/init.lua
@@ -39,6 +39,8 @@ require("lazy").setup({
             indent = { enable = true },  
         })
     end},
+    -- bottom status line 
+    { 'nvim-lualine/lualine.nvim', dependencies = { 'nvim-tree/nvim-web-devicons' } },
     { "nvim-tree/nvim-tree.lua", dependencies = { "nvim-tree/nvim-web-devicons" } },
     { "iamcco/markdown-preview.nvim" },
     { "catppuccin/nvim" },  -- Add the Catppuccin colorscheme
@@ -107,6 +109,49 @@ end
 vim.api.nvim_create_autocmd({ "VimEnter" }, { callback = open_nvim_tree })
 
 vim.cmd [[ autocmd VimEnter * ++nested wincmd w ]]
+
+-- lualine configuration
+require('lualine').setup {
+  options = {
+    icons_enabled = true,
+    theme = 'auto',
+    component_separators = { left = '', right = ''},
+    section_separators = { left = '', right = ''},
+    disabled_filetypes = {
+      statusline = {},
+      winbar = {},
+    },
+    ignore_focus = {},
+    always_divide_middle = true,
+    always_show_tabline = true,
+    globalstatus = false,
+    refresh = {
+      statusline = 100,
+      tabline = 100,
+      winbar = 100,
+    }
+  },
+  sections = {
+    lualine_a = {'mode'},
+    lualine_b = {'branch', 'diff', 'diagnostics'},
+    lualine_c = {'filename'},
+    lualine_x = {'encoding', 'fileformat', 'filetype'},
+    lualine_y = {'progress'},
+    lualine_z = {'location'}
+  },
+  inactive_sections = {
+    lualine_a = {},
+    lualine_b = {},
+    lualine_c = {'filename'},
+    lualine_x = {'location'},
+    lualine_y = {},
+    lualine_z = {}
+  },
+  tabline = {},
+  winbar = {},
+  inactive_winbar = {},
+  extensions = {}
+}
 
 -- Set colorscheme (catppuccin-mocha if available, default otherwise)
 local ok, _ = pcall(vim.cmd, 'colorscheme catppuccin-mocha')
