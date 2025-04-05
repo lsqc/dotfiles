77d2dc19950764537e0783e31d606f6ae6c367b9
lsdevcloud <info@lsdevcloud.com>
2025-04-05 17:42:07 +0200

    neovim: fix structure and plugin organization

---
+++ dot_config/nvim/lua/core/plugins.lua
@@ -0,0 +1,29 @@
+require("lazy").setup({
+  spec = {
+    { "shaunsingh/nord.nvim" },
+    { "nvim-treesitter/nvim-treesitter", build = ":TSUpdate" },
+    { "nvim-lualine/lualine.nvim", dependencies = { "nvim-tree/nvim-web-devicons" } },
+    { "nvim-tree/nvim-tree.lua", dependencies = { "nvim-tree/nvim-web-devicons" } },
+    {
+      "neovim/nvim-lspconfig",
+      lazy = false,
+      dependencies = {
+        { "ms-jpq/coq_nvim", branch = "coq" },
+        { "ms-jpq/coq.thirdparty", branch = "3p" },
+      },
+      init = function()
+        vim.g.coq_settings = { auto_start = true }
+      end,
+    },
+    {
+      "romgrk/barbar.nvim",
+      dependencies = {
+        "lewis6991/gitsigns.nvim",
+        "nvim-tree/nvim-web-devicons",
+      },
+      init = function() vim.g.barbar_auto_setup = false end,
+      version = "^1.0.0",
+    },
+  },
+  checker = { enabled = true },
+})
