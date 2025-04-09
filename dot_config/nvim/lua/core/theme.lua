d275b1aa27e216b22f13f2420806ea25394e48b9
lsdevcloud <info@lsdevcloud.com>
2025-04-09 16:03:09 +0200

    neovim: update theme.lua (customize line number colors)

--- dot_config/nvim/lua/core/theme.lua
+++ dot_config/nvim/lua/core/theme.lua
@@ -6,3 +6,8 @@ end
 vim.cmd('highlight Normal guibg=NONE ctermbg=NONE')
 vim.cmd('highlight NvimTreeNormal guibg=NONE ctermbg=NONE')
 vim.cmd('highlight NvimTreeVertSplit guibg=NONE ctermbg=NONE')
+
+-- set colors for line numbers
+vim.api.nvim_set_hl(0, 'LineNrAbove', { fg='#292929', bold=false })
+vim.api.nvim_set_hl(0, 'LineNr', { fg='#FB508F', bold=true })
+vim.api.nvim_set_hl(0, 'LineNrBelow', { fg='#292929', bold=false })
