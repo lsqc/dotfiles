4da341a3c92c20efe16b5ab1b1f278613e5f4595
lsdevcloud <info@lsdevcloud.com>
2025-04-09 17:29:38 +0200

    neovim: update `nvimtree.lua` (move file tree to the right)

--- dot_config/nvim/lua/plugins/nvimtree.lua
+++ dot_config/nvim/lua/plugins/nvimtree.lua
@@ -1,6 +1,9 @@
 require("nvim-tree").setup({
-  sort = { sorter = "case_sensitive" },
-  view = { width = 30 },
-  renderer = { group_empty = true },
-  filters = { dotfiles = false },
+    sort = { sorter = "case_sensitive" },
+    view = {
+        width = 30,
+        side = "right",
+    },
+    renderer = { group_empty = true },
+    filters = { dotfiles = false },
 })
