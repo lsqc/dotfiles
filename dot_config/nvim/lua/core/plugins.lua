15e3dfdf4b7084a09920de3cbc0690e759200381
lsqc <lsqc@lsdevcloud.net>
2025-04-16 00:53:36 +0200

    neovim: add `alpha` plugin

--- dot_config/nvim/lua/core/plugins.lua
+++ dot_config/nvim/lua/core/plugins.lua
@@ -16,6 +16,17 @@ require("lazy").setup({
             end,
         },
         {
+            "goolord/alpha-nvim",
+            dependencies = { 'nvim-tree/nvim-web-devicons' },
+            config = function()
+            local startify = require("alpha.themes.startify")
+            startify.file_icons.provider = "devicons"
+            require("alpha").setup(
+                startify.config
+            )
+            end,
+        },
+        {
             "romgrk/barbar.nvim",
             dependencies = {
                 "lewis6991/gitsigns.nvim",
