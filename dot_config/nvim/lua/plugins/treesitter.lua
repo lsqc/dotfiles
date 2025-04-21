68aee6d1e1e7abe00dcadd99daa12c4988330313
lsqc <lsqc@lsdevcloud.net>
2025-04-21 19:24:47 +0200

    neovim: add autocompletion for maven projects

--- dot_config/nvim/lua/plugins/treesitter.lua
+++ dot_config/nvim/lua/plugins/treesitter.lua
@@ -1,5 +1,5 @@
 require("nvim-treesitter.configs").setup({
-  ensure_installed = { "c", "lua", "vim", "vimdoc", "query", "elixir", "heex", "javascript", "html" },
+  ensure_installed = { "c", "lua", "vim", "vimdoc", "query", "elixir", "heex", "javascript", "html", "java", "rust"},
   sync_install = false,
   highlight = { enable = true },
   indent = { enable = true },
