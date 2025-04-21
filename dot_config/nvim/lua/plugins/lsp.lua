57db9b681a851e542b54324379fe5bcd1f8ebbb8
lsqc <lsqc@lsdevcloud.net>
2025-04-21 20:47:24 +0200

    neovim: fix formatting issues

--- dot_config/nvim/lua/plugins/lsp.lua
+++ dot_config/nvim/lua/plugins/lsp.lua
@@ -1,6 +1,21 @@
 local lspconfig = require("lspconfig")
 
 lspconfig.jdtls.setup {
-    cmd = { 'jdtls' }, 
-    root_dir = lspconfig.util.root_pattern('pom.xml', 'build.gradle', '.git'),
+    cmd = { "jdtls" },
+    root_dir = lspconfig.util.root_pattern("pom.xml", ".git"),
+    settings = {
+        java = {
+            contentProvider = { preferred = "fernflower" },
+            codeGeneration = {
+                toString = { template = "${objectClassName}(${memberName} = ${memberValue})" },
+            },
+            import = {
+                enable = true,  
+                organizeImports = true,
+            },
+            formatting = {
+                enabled = false,
+            }
+        },
+    },
 }
