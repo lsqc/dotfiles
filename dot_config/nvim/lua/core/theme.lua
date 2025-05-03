d7b53b8ead3875d4d056842cf55ee22acdcb5947
lsqc <lsqc@lsdevcloud.net>
2025-05-03 22:44:20 +0200

    neovim: update theme
    Set custom color for alpha header

--- dot_config/nvim/lua/core/theme.lua
+++ dot_config/nvim/lua/core/theme.lua
@@ -12,3 +12,6 @@ vim.cmd('highlight NvimTreeVertSplit guibg=NONE ctermbg=NONE')
 vim.api.nvim_set_hl(0, 'LineNrAbove', { fg='#292929', bold=false })
 vim.api.nvim_set_hl(0, 'LineNr', { fg='#FB508F', bold=true })
 vim.api.nvim_set_hl(0, 'LineNrBelow', { fg='#292929', bold=false })
+
+-- set color for alpha header
+vim.api.nvim_set_hl(0, "AlphaHeaderCustom", { fg = "#0ac963" }) 
