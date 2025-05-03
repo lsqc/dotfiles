97dd51dbf39f8e20a47c0f5b06dc3e62c7d9e0b5
lsqc <lsqc@lsdevcloud.net>
2025-05-03 22:05:09 +0200

    neovim: add telescope plugin

--- dot_config/nvim/lua/core/plugins.lua
+++ dot_config/nvim/lua/core/plugins.lua
@@ -75,6 +75,11 @@ require("lazy").setup({
                 })
             end,
             ft = { "java" },
+        },
+        {
+            'nvim-telescope/telescope.nvim', tag = '0.1.8',
+            -- or                              , branch = '0.1.x',
+            dependencies = { 'nvim-lua/plenary.nvim' }
         }
     },
     checker = { enabled = true }
