785a98c91bbc85f1b514ee002997e02330b527ec
lsqc <lsqc@lsdevcloud.net>
2025-05-04 15:18:49 +0200

    neovim: update alpha config
    add button for file tree

--- dot_config/nvim/lua/plugins/alpha.lua
+++ dot_config/nvim/lua/plugins/alpha.lua
@@ -12,9 +12,11 @@ dashboard.section.header.val = {
     "                                                     ",
 }
 
+-- set buttons
 dashboard.section.buttons.val = {
     dashboard.button( "n", "  new file" , ":ene <BAR> startinsert <CR>"),
     dashboard.button( "f", "  find file", ":cd $HOME/repos | Telescope find_files<CR>"),
+    dashboard.button( "t", "  open file tree", ":NvimTreeOpen <CR>"),
     dashboard.button( "r", "  recent"   , ":Telescope oldfiles<CR>"),
     dashboard.button( "q", "  quit", ":qa<CR>"),
 }
