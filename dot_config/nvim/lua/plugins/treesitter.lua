cd125dbd4726570a2324ae138764f0a5896ca50f
lsqc <lsqc@lsdevcloud.net>
2025-06-17 23:15:32 +0200

    neovim: update treesitter config
    disable treesitter indentation

--- dot_config/nvim/lua/plugins/treesitter.lua
+++ dot_config/nvim/lua/plugins/treesitter.lua
@@ -2,5 +2,5 @@ require("nvim-treesitter.configs").setup({
   ensure_installed = { "c", "lua", "vim", "vimdoc", "query", "elixir", "heex", "javascript", "html", "java", "rust"},
   sync_install = false,
   highlight = { enable = true },
-  indent = { enable = true },
+  indent = { enable = false },
 })
