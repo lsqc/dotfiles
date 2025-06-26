bfffcf31d86d651ad958b821962c512e38047a2a
lsqc <lsqc@lsdevcloud.net>
2025-06-26 17:58:09 +0200

    neovim: update lsp.lua
    add support for gradle projects

--- dot_config/nvim/lua/plugins/lsp.lua
+++ dot_config/nvim/lua/plugins/lsp.lua
@@ -2,7 +2,7 @@ local lspconfig = require("lspconfig")
 
 lspconfig.jdtls.setup {
     cmd = { "jdtls" },
-    root_dir = lspconfig.util.root_pattern("pom.xml", ".git"),
+    root_dir = lspconfig.util.root_pattern("settings.gradle", "pom.xml", ".git"),
     settings = {
         java = {
             contentProvider = { preferred = "fernflower" },
