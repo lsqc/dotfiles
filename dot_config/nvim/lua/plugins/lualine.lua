64a4abe63e3695104458067dfa17cf7a96b704ad
lsqc <lsqc@nya.vodka>
2025-10-13 01:09:54 +0200

    nvim: add lualine config

---
+++ dot_config/nvim/lua/plugins/lualine.lua
@@ -0,0 +1,25 @@
+return {
+    {
+        "nvim-lualine/lualine.nvim",
+        dependencies = { "nvim-tree/nvim-web-devicons" },
+        lazy = false,
+        config = function()
+            require("lualine").setup({
+                options = {
+                    theme = "tokyonight", 
+                    icons_enabled = true,
+                    section_separators = "",
+                    component_separators = "",
+                },
+                sections = {
+                    lualine_a = { "mode" },
+                    lualine_b = { "branch", "diff", "diagnostics" },
+                    lualine_c = { "filename" },
+                    lualine_x = { "encoding", "fileformat", "filetype" },
+                    lualine_y = { "progress" },
+                    lualine_z = { "location" },
+                },
+            })
+        end,
+    },
+}
